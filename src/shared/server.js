26942468d13faaff409b3b2cd01a2349a6de4d03
Carlos Fraga <vittorfraga@outlook.com>
2023-03-04 15:09:03 -0300

    alterando cors

--- src/shared/server.js
+++ src/shared/server.js
@@ -14,13 +14,12 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
